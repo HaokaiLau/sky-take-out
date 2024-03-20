9ecbea4807532af8f65497ea0496dc0362e4ac95
lhj <773849533@qq.com>
2024-03-20 23:04:30 +0800

    修复了后台令牌校验时不显示当前登录用户id的问题

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
@@ -48,7 +48,7 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
             Long UserId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
-            log.info("当前用户id：", UserId);
+            log.info("当前用户id:{}", UserId);
             BaseContext.setCurrentId(UserId);
             //3、通过，放行
             return true;
